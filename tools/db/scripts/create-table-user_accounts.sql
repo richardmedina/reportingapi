20b62481a7b190a2a85d139274e614cfe6105aa2
Ricardo Medina <ricki9@gmail.com>
2022-06-05 00:30:39 -0700

    added sql script create-table-user_accounts

---
+++ tools/db/scripts/create-table-user_accounts.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS `reportsdb`;
+USE `reportsdb`;
+
+DROP TABLE IF EXISTS `user_accounts`;
+
+CREATE TABLE `user_accounts` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `user_name` varchar(1024) DEFAULT '',
+    `password` varchar(1024) NOT NULL,
+    `first_name` varchar(1024) DEFAULT '',
+    `last_name` varchar(1024) DEFAULT '',
+	PRIMARY KEY(`id`)
+);
+
+INSERT INTO `users_accounts` (`user_name`, `password`, `first_name`, `last_name`) 
+	VALUES ('admin', 'admin', 'Administrator', 'Admin')
+
